f700e8999c00a44a82f7758674447080b17a20cc
Kolya Siryk <kolyasirik@gmail.com>
2022-08-18 18:42:27 +0300

    Create Auth0ModuleProvider.php

---
+++ Auth0ModuleProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LenderKit\Modules\Auth0\Providers;
+
+use LenderKit\Modules\Setup\Providers\BaseModuleServiceProvider;
+
+class Auth0ModuleProvider extends BaseModuleServiceProvider
+{
+
+}
